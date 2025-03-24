02addb21d9b1ee84efdf06e5c96abc7617a5b213
Shakhzoda <shakhzoda.ekubbekova@alatoo.edu.kg>
2025-03-24 16:33:30 +0600

    2.20

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/repositories/BookRepository.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/repositories/BookRepository.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6webapp.repositories;
 
+import guru.springframework.spring6webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
